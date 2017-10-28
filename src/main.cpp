89ba570cb92a3d856a2fe1e3d8684894a42d3842
Menginventor <meng.inventor@gmail.com>
2017-10-29 05:48:40 +0700

    Revert "litle change"
    This reverts commit d9e33b240a51a3c2e35e36be812b0804c77cb570.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 
 
 Ticker display_timer;
-float display_period = (1.0/10.0);
 void all_drive(float val){
         wheel1.write(val);
         wheel2.write(val);
@@ -20,8 +19,7 @@ void all_drive(float val){
 }
 void display(){
         //pc.printf("%d\t%d\t%d\n",wheel1_encoder.q_state,wheel2_encoder.q_state,wheel3_encoder.q_state);
-        //pc.printf("%d\t%d\t%d\n",wheel1_encoder.pos,wheel2_encoder.pos,wheel3_encoder.pos);
-        pc.printf("%f\t%f\n",(float(wheel1_encoder.pos)/display_period )/1000.0,wheel1_encoder.speed/1000.0);
+        pc.printf("%d\t%d\t%d\n",wheel1_encoder.pos,wheel2_encoder.pos,wheel3_encoder.pos);
         wheel1_encoder.pos = 0;
         wheel2_encoder.pos= 0;
         wheel3_encoder.pos= 0;
@@ -29,18 +27,16 @@ void display(){
 }
 int main() {
         pc.baud(115200);
-        display_timer.attach(&display,display_period );
+        display_timer.attach(&display,1.0/100.0);
 
 
         while(btn.read()==1) ;
-        all_drive(0.1);
         while(1) {
-/*
                 for(float i = 0.0; i<1.0; i+=0.01) {
                         all_drive(i);
                         wait(0.01);
                 }
-                wait(0.5);
+                //wait(0.5);
                 for(float i = 1.0; i>0; i-=0.01) {
                         all_drive(i);
                         wait(0.01);
@@ -49,12 +45,11 @@ int main() {
                         all_drive(i);
                         wait(0.01);
                 }
-                wait(0.5);
+                //wait(0.5);
                 for(float i = -1.0; i<0; i+=0.01) {
                         all_drive(i);
                         wait(0.01);
                 }
-*/
         }
 
 }
