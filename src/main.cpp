d9e33b240a51a3c2e35e36be812b0804c77cb570
Menginventor <meng.inventor@gmail.com>
2017-10-29 04:43:00 +0700

    litle change

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <conf.h>
 
 /*
-Prism group
-software for omni robot project
-start dev at 29/10/2017 (DD/MM/YYYY)
-If you develop this file , sign your name here.
-Dhamdhawach Horsuwan
-*/
+   Prism group
+   software for omni robot project
+   start dev at 29/10/2017 (DD/MM/YYYY)
+   If you develop this file , sign your name here.
+   Dhamdhawach Horsuwan
+ */
 
 
 Ticker display_timer;
@@ -18,11 +18,11 @@ void all_drive(float val){
         wheel3.write(val);
 }
 void display(){
-  //pc.printf("%d\t%d\t%d\n",wheel1_encoder.q_state,wheel2_encoder.q_state,wheel3_encoder.q_state);
-  pc.printf("%d\t%d\t%d\n",wheel1_encoder.pos,wheel2_encoder.pos,wheel3_encoder.pos);
-  wheel1_encoder.pos = 0;
-  wheel2_encoder.pos= 0;
-  wheel3_encoder.pos= 0;
+        //pc.printf("%d\t%d\t%d\n",wheel1_encoder.q_state,wheel2_encoder.q_state,wheel3_encoder.q_state);
+        pc.printf("%d\t%d\t%d\n",wheel1_encoder.pos,wheel2_encoder.pos,wheel3_encoder.pos);
+        wheel1_encoder.pos = 0;
+        wheel2_encoder.pos= 0;
+        wheel3_encoder.pos= 0;
 
 }
 int main() {
@@ -34,21 +34,21 @@ int main() {
         while(1) {
                 for(float i = 0.0; i<1.0; i+=0.01) {
                         all_drive(i);
-                        wait(0.025);
+                        wait(0.01);
                 }
-                wait(0.5);
+                //wait(0.5);
                 for(float i = 1.0; i>0; i-=0.01) {
                         all_drive(i);
-                        wait(0.025);
+                        wait(0.01);
                 }
                 for(float i = 0.0; i>-1.0; i-=0.01) {
                         all_drive(i);
-                        wait(0.025);
+                        wait(0.01);
                 }
-                wait(0.5);
+                //wait(0.5);
                 for(float i = -1.0; i<0; i+=0.01) {
                         all_drive(i);
-                        wait(0.025);
+                        wait(0.01);
                 }
         }
 
